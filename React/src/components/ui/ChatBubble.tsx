e6c7d5984548746d309b64d15844a0706843050d
Alan-CF <alanyaco.cf@gmail.com>
2026-04-21 17:40:47 -0600

    [REFACTOR] Bubble children

--- React/src/components/ui/ChatBubble.tsx
+++ React/src/components/ui/ChatBubble.tsx
@@ -1,17 +1,17 @@
 export interface ChatMessage {
-  message: string;
+  children: React.ReactNode;
   isUser: boolean;
 }
 
-export default function ChatBubble({ message, isUser }: ChatMessage) {
+export default function ChatBubble({ children, isUser }: ChatMessage) {
   return (
     <div className={`flex ${isUser ? 'justify-end' : 'justify-start'} mb-2`}>
       <div className={`max-w-xs px-4 py-2 text-sm rounded-2xl
         ${isUser
-          ? 'bg-secondary text-white rounded-tr-none'
-          : 'bg-gray-200 text-gray-900 rounded-tl-none'
+          ? 'bg-secondary text-white rounded-tr-none ml-5'
+          : 'bg-gray-100 text-black border border-gray-300 rounded-tl-none mr-5'
         }`}>
-        {message}
+        {children}
       </div>
     </div>
   );
