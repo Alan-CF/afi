fde2eb6adc1c335b98888ba196280b0a87779571
Alan-CF <alanyaco.cf@gmail.com>
2026-05-05 14:47:00 -0600

    [FIX] Room layout

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -39,8 +39,6 @@ function App() {
           <Route path="/profile/:profileId" element={<PublicProfile />} />
           <Route path="/rooms" element={<Rooms />} />
           <Route path="/rooms/create" element={<CreateRoom />} />
-          <Route path="/rooms/chat" element={<RoomChat />} />
-          <Route path="/rooms/:roomId" element={<RoomChat />} />
           <Route path="/ranking" element={<Ranking />} />
           <Route path="/shop" element={<Shop />} />
           <Route path="/shop/products" element={<ShopProducts />} />
@@ -51,6 +49,9 @@ function App() {
           <Route path="/news" element={<News />} />
           <Route path="/news/:slug" element={<NewsDetail />} />
         </Route>
+
+        <Route path="/rooms/chat" element={<RoomChat />} />
+        <Route path="/rooms/:roomId" element={<RoomChat />} />
       </Routes>
     </>
   );
