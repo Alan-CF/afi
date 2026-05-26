56a94b8245049e6f89566c69ec7f526c4e426b9b
Choche Sanchez <chochesanchez2003@gmail.com>
2026-05-26 04:17:16 -0600

    change text color gray -> blue

--- React/src/components/home/HomeEventsSection.tsx
+++ React/src/components/home/HomeEventsSection.tsx
@@ -30,7 +30,7 @@ export default function HomeEventsSection() {
         </Link>
         <Link
           to="/events"
-          className="font-lato text-sm font-bold text-text-light hover:text-secondary transition-colors shrink-0"
+          className="font-lato text-sm font-bold text-secondary hover:text-[#5780AE] transition-colors shrink-0"
         >
           See all
         </Link>
--- React/src/components/home/HomeTopNews.tsx
+++ React/src/components/home/HomeTopNews.tsx
@@ -16,7 +16,7 @@ function SectionHeader() {
       </Link>
       <Link
         to="/news"
-        className="font-lato text-sm font-bold text-text-light hover:text-secondary transition-colors shrink-0"
+        className="font-lato text-sm font-bold text-secondary hover:text-[#5780AE] transition-colors shrink-0"
       >
         See all
       </Link>
