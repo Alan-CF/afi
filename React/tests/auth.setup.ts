be83c6cd15136194d04c7e5d0e2b1f105e1eaec4
Alan-CF <alanyaco.cf@gmail.com>
2026-05-30 19:09:21 -0600

    [FIX] TS config

--- React/tests/auth.setup.ts
+++ React/tests/auth.setup.ts
@@ -28,7 +28,7 @@ setup('auth', async ({ page }) => {
 
 async function getSupabaseSession() {
   const supabaseUrl = process.env.VITE_SUPABASE_URL;
-  const supabaseAnonKey = process.env.VITE_SUPABASE_ANON_KEY;
+  const supabaseAnonKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
   const testUserEmail = process.env.TEST_USER_EMAIL;
   const testUserPassword = process.env.TEST_USER_PASSWORD;
