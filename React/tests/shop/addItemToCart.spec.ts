bbff45fbde2960abbc859ff54d1bbb32700cefea
Alan-CF <alanyaco.cf@gmail.com>
2026-06-02 19:51:32 -0600

    [REFACTOR] Cart management

--- React/tests/shop/addItemToCart.spec.ts
+++ React/tests/shop/addItemToCart.spec.ts
@@ -1,6 +1,9 @@
 import { test, expect } from '@playwright/test';
-import { supabase } from '../helpers/supabaseClient';
-import { createMockProduct, deleteMockProduct } from './crud/MockProduct';
+import {
+  createMockPricedProduct,
+  deleteMockPricedProduct,
+} from './crud/MockPricedProduct';
+import { clearCart } from './crud/Cart';
 
 test('Add item to cart', async ({ page }) => {
   await dbSetup();
@@ -28,57 +31,10 @@ test.afterEach(async () => {
 });
 
 async function dbSetup() {
-  const productData = await createMockProduct();
-
-  const { data: priceData, error: priceError } = await supabase
-    .from('product_pricing')
-    .insert({
-      product_id: productData.id,
-      price: 99.99,
-      discount: 0.1,
-    })
-    .select()
-    .single();
-  console.log('insert result:', priceData, priceError);
-  if (priceError) throw new Error(`DB setup failed: ${priceError.message}`);
+  await createMockPricedProduct();
 }
 
 async function dbTeardown() {
-  // 1. Obtener el producto
-  const { data: product, error: productSelError } = await supabase
-    .from('product_catalog')
-    .select('id')
-    .eq('name', 'Test Product')
-    .single();
-  console.log('select product:', product, productSelError);
-  if (productSelError)
-    throw new Error(`DB teardown failed: ${productSelError.message}`);
-
-  // 2. Obtener el precio del producto
-  const { data: pricedProduct, error: pricedProductSelError } = await supabase
-    .from('product_pricing')
-    .select('id')
-    .eq('product_id', product.id)
-    .single();
-  console.log('select pricing:', pricedProduct, pricedProductSelError);
-  if (pricedProductSelError)
-    throw new Error(`DB teardown failed: ${pricedProductSelError.message}`);
-
-  // 3. Borrar cart
-  const { error: cartError } = await supabase
-    .from('shopping_cart_items')
-    .delete()
-    .eq('priced_product_id', pricedProduct.id);
-  console.log('delete cart error:', cartError);
-  if (cartError) throw new Error(`DB teardown failed: ${cartError.message}`);
-
-  // 4. Borrar precio
-  const { error: priceError } = await supabase
-    .from('product_pricing')
-    .delete()
-    .eq('product_id', product.id);
-  console.log('delete pricing error:', priceError);
-  if (priceError) throw new Error(`DB teardown failed: ${priceError.message}`);
-
-  await deleteMockProduct();
+  await clearCart();
+  await deleteMockPricedProduct();
 }
