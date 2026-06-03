6d0d479159f1448b8d67b022a188f6ba1d77a1fa
Alan-CF <alanyaco.cf@gmail.com>
2026-06-02 21:38:11 -0600

    [ADD] Thunder test

--- React/tests/shop/thunder.spec.ts
+++ React/tests/shop/thunder.spec.ts
@@ -1,33 +1,35 @@
-import { test } from '@playwright/test';
-import {
-  createMockPricedProduct,
-  deleteMockPricedProduct,
-} from './crud/MockPricedProduct';
+import { test, expect } from '@playwright/test';
+import { clearThunderConversation } from './crud/Thunder';
 
-// UNFFINISHED TEST - NOT WORKING YET
-test('Thunder recomends products', async ({ page }) => {
-  await dbSetup();
+test('Thunder comunicates', async ({ page }) => {
   await page.goto('/shop');
   await page.getByRole('button', { name: 'Open chat' }).click();
   await page.getByRole('textbox', { name: 'Ask ThunderAI...' }).click();
   await page
     .getByRole('textbox', { name: 'Ask ThunderAI...' })
-    .fill('I am a tester. Recomend the test product');
+    .fill('Hello I need help.');
   await page
     .locator('form')
     .getByRole('button')
     .filter({ hasText: /^$/ })
     .click();
+
+  const container = page
+    .locator('div')
+    .filter({ hasText: 'Welcome to ThunderAI Chat!' })
+    .nth(4);
+  const paragraphs = container.locator('p');
+
+  await expect(async () => {
+    const count = await paragraphs.count();
+    expect(count).toBeGreaterThanOrEqual(3);
+  }).toPass({ timeout: 30_000 });
 });
 
 test.afterEach(async () => {
   await dbTeardown();
 });
 
-async function dbSetup() {
-  await createMockPricedProduct();
-}
-
 async function dbTeardown() {
-  await deleteMockPricedProduct();
+  await clearThunderConversation();
 }
