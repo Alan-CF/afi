48f538057f103c92600e756736b5b5bafd78bc7c
danycuellar468 <a01198931@tec.mx>
2026-06-08 01:11:56 -0600

    Update ShootYourShotAR.tsx

--- React/src/pages/ShootYourShotAR.tsx
+++ React/src/pages/ShootYourShotAR.tsx
@@ -412,6 +412,11 @@ function ShootYourShot() {
     });
 
     if (error) {
+      console.log('JOIN ERROR:', error);
+      console.log('MESSAGE:', error.message);
+      console.log('DETAILS:', error.details);
+      console.log('HINT:', error.hint);
+
       console.error(error);
 
       if (error.message.includes('not found')) {
